8973b0f31bba2a32a5b8d9f494f514c07a78e040
Guilherme Rocha <gui1802@hotmail.com>
2019-12-07 23:57:09 -0300

    Projeto criado

---
+++ src/main/java/br/com/exemplo/api/ApiCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarrosApplication.class, args);
+	}
+
+}
