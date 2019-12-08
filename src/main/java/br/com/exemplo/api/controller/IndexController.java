76644d0923c8f9a88b6048f32e1f04bf7b891a20
Guilherme Rocha <gui1802@hotmail.com>
2019-12-08 01:44:20 -0300

    Como manipular parametros e urls com a anotação @GetMapping

--- src/main/java/br/com/exemplo/api/controller/IndexController.java
+++ src/main/java/br/com/exemplo/api/controller/IndexController.java
@@ -1,8 +1,8 @@
 package br.com.exemplo.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +14,19 @@ public class IndexController {
 		return "Get Spring Boot";
 	}
 	
-	@GetMapping("/login")
-	public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+	@GetMapping("/login/{login}/senha/{senha}")
+	public String login(@PathVariable("login") String login, @PathVariable("senha") String senha) {
 		return "Login " + login +", senha:" + senha;
+	}
+	
+	@GetMapping("/carros/{id}")
+	public String getCarroById(@PathVariable("id") Long id) {
+		return "Carro by id " + id;
+	}
+	
+	@GetMapping("/carros/tipo/{tipo}")
+	public String getCarroByTipo(@PathVariable("tipo") String tipo) {
+		return "Carro by id " + tipo;
 	}
 
 
