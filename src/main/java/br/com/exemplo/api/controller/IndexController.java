ef17db6f555975448946035e0b86ec21638e93a1
Guilherme Rocha <gui1802@hotmail.com>
2019-12-08 01:16:50 -0300

    Como ler parametros com a anotação @RequestParam

--- src/main/java/br/com/exemplo/api/controller/IndexController.java
+++ src/main/java/br/com/exemplo/api/controller/IndexController.java
@@ -1,10 +1,8 @@
 package br.com.exemplo.api.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,17 +13,11 @@ public class IndexController {
 	public String get() {
 		return "Get Spring Boot";
 	}
-	@PostMapping
-	public String post() {
-		return "Post Spring Boot";
+	
+	@GetMapping("/login")
+	public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+		return "Login " + login +", senha:" + senha;
 	}
-	@PutMapping
-	public String put() {
-		return "Put Spring Boot";
-	}
-	@DeleteMapping
-	public String delete() {
-		return "Delete Spring Boot";
-	}
+
 
 }
