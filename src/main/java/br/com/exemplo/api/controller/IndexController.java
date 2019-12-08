c36b53bafbc2a3fb9b544cfec4f7a18308e7b3e8
Guilherme Rocha <gui1802@hotmail.com>
2019-12-08 10:19:11 -0300

    Envio de requisição via post

--- src/main/java/br/com/exemplo/api/controller/IndexController.java
+++ src/main/java/br/com/exemplo/api/controller/IndexController.java
@@ -2,6 +2,7 @@ package br.com.exemplo.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,20 +15,9 @@ public class IndexController {
 		return "Get Spring Boot";
 	}
 	
-	@GetMapping("/login/{login}/senha/{senha}")
+	@PostMapping("/login/{login}/senha/{senha}")
 	public String login(@PathVariable("login") String login, @PathVariable("senha") String senha) {
 		return "Login " + login +", senha:" + senha;
 	}
-	
-	@GetMapping("/carros/{id}")
-	public String getCarroById(@PathVariable("id") Long id) {
-		return "Carro by id " + id;
-	}
-	
-	@GetMapping("/carros/tipo/{tipo}")
-	public String getCarroByTipo(@PathVariable("tipo") String tipo) {
-		return "Carro by id " + tipo;
-	}
-
 
 }
